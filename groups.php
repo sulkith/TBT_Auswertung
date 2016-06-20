75c3db3cce5e3650509d7fc594a9405cc9dc1efc
Michael Drost <sulkith@gmail.com>
2016-06-20 20:22:43 +0200

    optimized Group View For Groups > 15

--- groups.php
+++ groups.php
@@ -12,7 +12,10 @@
 ?>
 
 <?php
-	createGroupTable(3,5)
+	//TODO Optimize for mobile use
+	$x = 3;
+	$y = MAX(5,(getMaxGroup()+2)/3);
+	createGroupTable($x,$y)
 ?>
 
 <?php $ArcherSymbolsLegend=1; include_once("projectspecific/template_foot.php");?>
